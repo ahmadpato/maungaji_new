7c4a123a435c5d0dbfc7fba56092a6a98d2c6cb9
hidayaturrohman <muhamad.hidayaturrohman@digisiber.co.id>
2020-06-20 13:21:51 +0700

    cleaning code

--- js/count.js
+++ js/count.js
@@ -5,22 +5,22 @@ $.get(url, function(res){
 	let data = res.data
 
 	$.each(data, function( key, value ) {
-	console.log(data)	
+
 	if(value.name == 'SANTRI'){
 	
-		document.getElementById("student").innerHTML	= value.name;
+		document.getElementById("student").innerHTML = value.name;
 
-		document.getElementById("count").innerHTML  	= value.count;
+		document.getElementById("count").innerHTML   = value.count;
 	
 	} else if(value.name == 'GURU NGAJI'){
 	
-		document.getElementById("teacher").innerHTML	= value.name;
+		document.getElementById("teacher").innerHTML	  = value.name;
 
-		document.getElementById("teacherCount").innerHTML  	= value.count;	
+		document.getElementById("teacherCount").innerHTML = value.count;	
 
 	} else{
 
-		document.getElementById("meet").innerHTML	= value.name;
+		document.getElementById("meet").innerHTML	   = value.name;
 
 		document.getElementById("meetCount").innerHTML = value.count;
 
