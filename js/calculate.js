ac0c3e748ce804a311b783a163d22877cc0ea8f9
hidayaturrohman <muhamad.hidayaturrohman@digisiber.co.id>
2020-06-20 11:30:47 +0700

    new pricing

--- js/calculate.js
+++ js/calculate.js
@@ -1,3 +1,4 @@
+ /*Online Calculate*/
 function calculateAmount(val) {
     var tot_price_personnel     = val * 25000;
     var reverse                 = tot_price_personnel.toString().split('').reverse().join(''),
@@ -30,3 +31,39 @@ function calculateAmountFamily(val) {
     var divobj = document.getElementById('tot_amount_group');
     divobj.value = 'Rp. ' + ribuan;
 }
+ /*End Online Calculate*/
+
+ /*Offline Calculate*/
+ function calculateAmountOffline(val) {
+    var tot_price_personnel_off = val * 100000;
+    var reverse                 = tot_price_personnel_off.toString().split('').reverse().join(''),
+     ribuan                     = reverse.match(/\d{1,3}/g);
+     ribuan                     = ribuan.join('.').split('').reverse().join('');
+
+    /*display the result*/
+    var divobj = document.getElementById('tot_amount_personnel_offline');
+    divobj.value = 'Rp. ' + ribuan;
+}
+
+function calculateAmountFamilyOffline(val) {
+    var tot_price_family_offline        = val * 125000;
+    var reverse                         = tot_price_family_offline.toString().split('').reverse().join(''),
+     ribuan                             = reverse.match(/\d{1,3}/g);
+     ribuan                             = ribuan.join('.').split('').reverse().join('');
+
+    /*display the result*/
+    var divobj = document.getElementById('tot_amount_family_offline');
+    divobj.value = 'Rp. ' + ribuan;
+}
+
+ function calculateAmountGroupOffline(val) {
+    var tot_price_group_offline         = val * 150000;
+    var reverse                         = tot_price_group_offline.toString().split('').reverse().join(''),
+     ribuan                             = reverse.match(/\d{1,3}/g);
+     ribuan                             = ribuan.join('.').split('').reverse().join('');
+
+    /*display the result*/
+    var divobj = document.getElementById('tot_amount_group_offline');
+    divobj.value = 'Rp. ' + ribuan;
+}
+ /*End Offline Calculate*/
